1f11b5a99c4b668501cad53578fbba9466209698
theeeguyyyy <138809290+theeeguyyyy@users.noreply.github.com>
2023-07-08 10:25:12 -0400

    Creating base health

---
+++ src/server/Base.server.lua
@@ -0,0 +1,15 @@
+local path = workspace:WaitForChild("Path")
+local base = path:WaitFOrChild("Base")
+
+local hits = {}
+
+base.Touched:Connect(function(hit)
+    if hit.Parent:FindFirstChild("Humanoid") and not game:GetService("Players"):GetPlayerFromCharacter(hit.Parent) then
+        if not table.find(hits, hit.Parent) then
+            table.insert(hits, hit.Parent)
+            base.Health.Value -= hit.Parent.Humanoid.Health
+            if base.Health.Value > 0 then base.Health.Value = 0 end
+            hit.Parent:Destroy()
+        end
+    end
+end)
