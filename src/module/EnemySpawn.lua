91ee5bddb0c80b65ae410f05ba53ecb996988b17
theeeguyyyy <138809290+theeeguyyyy@users.noreply.github.com>
2023-07-11 14:12:56 -0400

    Enemies can no longer collide with eachother

--- src/module/EnemySpawn.lua
+++ src/module/EnemySpawn.lua
@@ -1,25 +1,36 @@
 local module = {}
 
-function module.SpawnRegular(mob, amount, spawnInterval)
-    if not amount then amount = 1 end
+local PhysicsService = game:GetService("PhysicsService")
 
-    for i = 1, amount, 1 do
-        local char = mob:Clone()
-        char:WaitForChild("HumanoidRootPart").CFrame = workspace.Path.NormalWaypoints.First.CFrame
-        char.Parent = workspace.Enemies
-        task.spawn(function()
-            for i = 1, #workspace.Path.NormalWaypoints:GetChildren(), 1 do
-                for _, waypoint in pairs(workspace.Path.NormalWaypoints:GetChildren()) do
-                    if waypoint.Waypoint.Value == i and i ~= 1 then
-                        char:WaitForChild("Humanoid"):MoveTo(waypoint.Position)
-                        char.NextPos.Value = waypoint.Position
-                        char.Humanoid.MoveToFinished:Wait()
-                    end
-                end
-            end
-        end)
-        if spawnInterval then task.wait(spawnInterval) end
-    end
-end
+PhysicsService:CreateCollisionGroup("Enemies")
+PhysicsService:CollisionGroupsAreCollidable("Enemies", "Enemies", false)
+
+	function module.SpawnRegular(mob, amount, spawnInterval)
+		if not amount then amount = 1 end
+
+		for i = 1, amount, 1 do
+		local char = mob:Clone()
+		char:WaitForChild("HumanoidRootPart").CFrame = workspace.Path.NormalWaypoints.First.CFrame
+		char.HumanoidRootPart:SetNetworkOwner(nil)
+		for _, part in pairs(char:GetChildren()) do
+			if part:IsA("BasePart") or part:IsA("MeshPart") then
+				PhysicsService:SetPartCollisionGroup("Enemies", part)
+			end
+		end
+		char.Parent = workspace.Enemies
+		task.spawn(function()
+			for i = 1, #workspace.Path.NormalWaypoints:GetChildren(), 1 do
+				for _, waypoint in pairs(workspace.Path.NormalWaypoints:GetChildren()) do
+					if waypoint.Waypoint.Value == i and i ~= 1 then
+						char:WaitForChild("Humanoid"):MoveTo(waypoint.Position)
+						char.NextPos.Value = waypoint.Position
+						char.Humanoid.MoveToFinished:Wait()
+					end
+				end
+			end
+		end)
+		if spawnInterval then task.wait(spawnInterval) end
+	end
+	end
 
 return module
