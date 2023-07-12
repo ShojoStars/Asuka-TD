52b195f9d19ac0be93c7c9b633c70e4f96992eee
MasterDaniel <domonicdawkins24@gmail.com>
2023-07-11 22:53:56 -0500

    base Setup for Handling Rewards

---
+++ src/server/Waves.server.lua
@@ -0,0 +1,16 @@
+local Wave = 0
+local RewardsHandler = require(game.ReplicatedStorage.module.RewardsHandler)
+local Rewards = 
+{
+    Normal = 150,
+    Corruption = 250,
+    Nightmare = 500,
+    AllStar = 800,
+}
+
+--// To Reward Player use function
+--[[
+
+    RewardsHandler.AddCoins(Player,Rewards.Normal)
+    
+--]]
