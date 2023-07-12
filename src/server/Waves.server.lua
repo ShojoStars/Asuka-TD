63a14f3f9ca1e85bdefe7849f7bd9158ca01fe1a
MasterDaniel <domonicdawkins24@gmail.com>
2023-07-12 16:49:37 -0500

    Base Wave System

--- src/server/Waves.server.lua
+++ src/server/Waves.server.lua
@@ -1,16 +1,58 @@
-local Wave = 0
-local RewardsHandler = require(game.ReplicatedStorage.module.RewardsHandler)
-local Rewards = 
-{
-    Normal = 150,
-    Corruption = 250,
-    Nightmare = 500,
-    AllStar = 800,
+local RPS = game:GetService("ReplicatedStorage")
+local RewardsHandler = require(RPS.module.RewardsHandler)
+local EnemySpawnModule = require(RPS.EnemySpawnModule)
+
+local Rewards = {
+	Normal = 150,
+	Corruption = 250,
+	Nightmare = 500,
+	AllStar = 800,
 }
 
---// To Reward Player use function
---[[
+local Waves = {
+	Normal = {
+		WaveCount = 40,
+		Reward = Rewards.Normal,
+		Enemies = {
+			"Zakena",
+			"Zakena Fast",
+			"Zakena Tank",
+			"Zakena Big",
+			"Zakena Boss",
+		},
+	},
+}
 
-    RewardsHandler.AddCoins(Player,Rewards.Normal)
-    
---]]
+-- Fetch necessary objects and elements
+local EnemiesFolder = RPS:WaitForChild("Enemies")
+local path = workspace:WaitForChild("Path")
+local base = path:WaitForChild("Base")
+
+-- Function to start the wave
+local function StartWave(waveType)
+	local wave = 1 -- Initialize the wave counter
+
+	while wave <= waveType.WaveCount and base.Health.Value > 0 do
+		for i, enemyName in ipairs(waveType.Enemies) do
+			-- Find the enemy by name in the EnemiesFolder
+			local enemy = EnemiesFolder:FindFirstChild(enemyName, true)
+			if enemy then
+				-- Spawn the enemy using the EnemySpawnModule
+				EnemySpawnModule.SpawnRegular(enemy, 5, 1)
+			else
+				warn("Enemy not found:", enemyName)
+			end
+		end
+
+		wave = wave + 1 -- Increment the wave counter
+	end
+
+	if base.Health.Value == 0 then
+		warn("Game Failed")
+	else
+		warn("You Beat the Mode!")
+	end
+end
+
+-- Start the wave for the "Normal" wave type
+StartWave(Waves.Normal)
