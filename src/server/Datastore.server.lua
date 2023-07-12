a2fa77f7479bf89c95e4f8748edb1be52e5637ad
MasterDaniel <domonicdawkins24@gmail.com>
2023-07-11 22:42:23 -0500

    Changed Coins to Yen

--- src/server/Datastore.server.lua
+++ src/server/Datastore.server.lua
@@ -1,5 +1,5 @@
 local DataStoreService = game:GetService("DataStoreService")
-local DataStore = DataStoreService:GetDataStore("Asuka-Coins-Save")
+local DataStore = DataStoreService:GetDataStore("Asuka-Yen-Save")
 
 game.Players.PlayerAdded:Connect(function(player)
     local Stats = Instance.new("Folder")
@@ -8,17 +8,17 @@ game.Players.PlayerAdded:Connect(function(player)
 
     --// Add / Create new values here
 
-    local Coins = Instance.new("IntValue")
-    Coins.Name = "Coins"
-    Coins.Value = 0
-    Coins.Parent = Stats
+    local Yen = Instance.new("IntValue")
+    Yen.Name = "Yen"
+    Yen.Value = 0
+    Yen.Parent = Stats
 
-    --//Load the Coins
+    --//Load the Yen
 
     pcall(function(sucess,error)
         if sucess then
             --// Add More if Neccessary
-            Coins.Value = DataStore:GetAsync(player.UserId)
+            Yen.Value = DataStore:GetAsync(player.UserId)
         elseif error then
             warn("Error Getting Player Data or Player Has No Data")
         end
@@ -28,7 +28,7 @@ game.Players.PlayerAdded:Connect(function(player)
         ---// Saves Player Data
         pcall(function(sucess,error)
             if sucess then
-                DataStore:SetAsync(player.UserId,Coins.Value)
+                DataStore:SetAsync(player.UserId,Yen.Value)
             elseif error then
                 warn("Could Not Save Player Data!")
             end
