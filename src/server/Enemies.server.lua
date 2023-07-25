589ec98863f99a496f444cb27f9195181971253a
MasterDaniel <domonicdawkins24@gmail.com>
2023-07-25 15:20:51 -0500

    removed uneccesary calls

--- src/server/Enemies.server.lua
+++ src/server/Enemies.server.lua
@@ -8,5 +8,4 @@ local modules = RS:WaitForChild("Shared")
 local spawnModule = require(modules:WaitForChild("EnemySpawn"))
 local towerModule = require(modules:WaitForChild("Towers"))
 
-spawnModule.SpawnRegular(enemies:WaitForChild("TestingDummy"), 10, 0.5)
 towerModule.SpawnTest(CFrame.new(Vector3.new(876.924, 1.541, 7.049), Vector3.new(0, 0, 0)))
