dca3ef6759226bdf8f97eab8ee00b43904b836aa
Daniel <domonicdawkins24@gmail.com>
2023-08-13 08:44:51 -0500

    Removed Game Files

--- src/server/TowerServer.server.lua
+++
@@ -1,9 +0,0 @@
---//Handles Tower Spawning on the Server.
-
-local Event = game.ReplicatedStorage.Variables.TowerEvent
-local TowersModule = require(script.Towers)
-Event.OnServerEvent:Connect(function(Player,Tower,Position)
-    if Tower then
-        TowersModule.SpawnTower(Tower,Position)
-    end
-end)
