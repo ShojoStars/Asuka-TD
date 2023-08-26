07c43cec86dba1c502b0148c81633962dfefacd4
MasterDaniel <domonicdawkins24@gmail.com>
2023-08-25 21:24:06 -0500

    Updated Matchmaking didnt save

--- src/server/MatchMaking.server.lua
+++ src/server/MatchMaking.server.lua
@@ -1,23 +1,52 @@
-local TeleportService = game:GetService("TeleportService")
 --// Initial Game Setup
-
+local DataStoreService = game:GetService("DataStoreService")
+local TeleportService = game:GetService("TeleportService")
 local RemoteEvent = Instance.new("RemoteEvent")
-RemoteEvent.Parent = game.ReplicatedStorage
+RemoteEvent.Parent = game.ReplicatedStorage.Variables
 RemoteEvent.Name = "MatchMakingEvent"
+local Data
 
-local PlaceIDS = 
-{
-    Apartment = {Id = 14421495807,Name = "Nagis Apartment"},
+local PlaceIDS = {
+	Apartment = { Id = 14421495807, Name = "Nagis Apartment" },
 }
 
+local GameData = {
+	EquippedTowers = {},
+	OwnedTowers = {},
+	MaxTowers = 8,
+	Yen = nil,
+}
+
+local Towers = {}
 --//
 
-RemoteEvent.OnServerEvent:Connect(function(player,Map)
-    if player and Map ~= nil then
-        for index, Place in PlaceIDS do
-            if Place.Name == Map then
-                TeleportService:Teleport(Place.Id,player)
-            end
-        end
-    end
+--// Few things for the Inventory
+local Datastore = DataStoreService:GetDataStore("PlayersOwnedTowers")
+game.Players.PlayerAdded:Connect(function(player)
+	Data = Datastore:GetAsync(player.UserId)
+	if Data then
+		GameData.OwnedTowers = Data
+		RemoteEvent:FireClient(player, Data)
+	else
+		--// Attempt to Create Data
+		warn("First Time Player Attempting to Create Data")
+		table.insert(Towers, { "Cure Black", "Cure White" })
+		Datastore:SetAsync(player.UserId, Towers)
+		Data = Datastore:GetAsync(player.UserId)
+		GameData.OwnedTowers = Data
+		RemoteEvent:FireClient(player, Data)
+	end
 end)
+--//
+
+--// Handles Teleporting
+RemoteEvent.OnServerEvent:Connect(function(player, Map)
+	if player and Map ~= nil then
+		for index, Place in PlaceIDS do
+			if Place.Name == Map then
+				TeleportService:Teleport(Place.Id, player, GameData)
+			end
+		end
+	end
+end)
+--//
