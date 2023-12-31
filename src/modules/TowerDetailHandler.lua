a2ba2c08b4269a2b7afa8119a822fe972bd18525
Gorduxo <155206864+Gorduxo@users.noreply.github.com>
2023-12-31 02:05:45 +0000

    Testing Commit

---
+++ src/modules/TowerDetailHandler.lua
@@ -0,0 +1,10 @@
+local self = {
+	["Cure Black"] = {
+		Description = "Powerfull character can do cool things with magic!!";
+	};
+	["Cure White"] = {
+		Description = "Powerfull character can do cool things with strength!!";
+	};
+}
+
+return self
