894511aa4b8a8e95201e5c656e5f9cd5fbb706ec
Gorduxo <155206864+Gorduxo@users.noreply.github.com>
2024-01-06 03:39:08 +0000

    New Matching System
    Info:
    Code works on the server!
    
    Location:
    Serverscriptservice > Server
    
    Description:
    when the player sits on the chair, and if there is no one else sitting he will be defined as the owner, if there are players in the same seat as the owner and he leaves, the other players will jump from the chairs, like a forced jump.

---
+++ src/serverscriptservice/Modules/Datastore/init.lua
@@ -0,0 +1,112 @@
+--//Service
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+local DataStoreService = game:GetService("DataStoreService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+
+--//Directories
+local Remotes_Folder = ReplicatedStorage:WaitForChild("Remotes")
+local Modules_Folder = ReplicatedStorage:WaitForChild("Modules")
+
+--//Assigments
+local DataName = "Data_"
+local DataVersion = "001"
+
+--//Objects
+local GameData = DataStoreService:GetDataStore(DataName..DataVersion)
+local PlayerData = script:WaitForChild("PlayerData")
+
+--//Local Function
+local function CreatePlayerData(Player)
+	
+	--/Clone and put inside of player in Players
+	local NewData = PlayerData:Clone()
+	NewData.Parent = Player
+	
+	--/return function with DataPlayer
+	return NewData
+end
+
+--//Functions
+local self = {}
+
+function self:LoadData(Player,Data)
+	
+	--/Assigments
+	local CurrentData;
+	
+	--/Create Data
+	local PlayerData = CreatePlayerData(Player)
+	
+	--/Check if got sucess getting current data
+	local sucess, fail = pcall(function()
+		CurrentData = GameData:GetAsync(Player.UserId)
+	end)
+	
+	--/confirm if have data or no
+	if sucess and CurrentData then
+		
+		for FolderName,Value in CurrentData do
+			
+			local FindFolder = PlayerData:FindFirstChild(FolderName)
+			if FindFolder and FindFolder:IsA("Folder") then
+				for ValueName,Value in Value do
+					if typeof(Value) == "boolean" then
+						local NewValue = Instance.new("BoolValue")
+						NewValue.Name = ValueName
+						NewValue.Value = Value
+						NewValue.Parent = FindFolder
+					end
+				end
+			elseif FindFolder and FindFolder:IsA("NumberValue") and CurrentData[FolderName] then
+				FindFolder.Value = CurrentData[FolderName]
+			end
+		end
+		
+		--/Return data
+		return CurrentData
+	else
+		return warn("New Player, creating new datastore")
+	end
+end
+
+function self:SaveData(Player)
+	
+	local Data = {}
+	
+	for DataNumber,DataName in Player:WaitForChild("PlayerData"):GetChildren() do
+		if not Data[DataName.Name] then
+			
+			Data[DataName.Name] = {}
+
+			if DataName:IsA("Folder") and #DataName:GetChildren() > 0 then
+				for DataValueNumber,DataValue in DataName:GetChildren() do
+					Data[DataName.Name][DataValue.Name] = DataValue.Value
+				end
+			elseif not DataName:IsA("Folder") then
+				Data[DataName.Name] = DataName.Value
+			end
+		end
+	end
+	
+	if Data then
+		
+		--/Check if got sucess getting current data
+		local sucess, fail = pcall(function()
+			return GameData:SetAsync(Player.UserId,Data)
+		end)
+
+		if sucess then
+			return warn("Data saved with sucess")
+		else
+			return error("Failed to save data player ("..fail..")")
+		end
+	end
+end
+
+function self:CheckData(Player,Data)
+	
+end
+
+return self
--- src/serverscriptservice/Server/MatchmarkingEvent.server.lua
+++ src/serverscriptservice/Server/MatchmarkingEvent.server.lua
@@ -1,28 +1,110 @@
---// Initial Game Setup
-local DataStoreService = game:GetService("DataStoreService")
-local TeleportService = game:GetService("TeleportService")
-local RemoteEvent = Instance.new("RemoteEvent")
-RemoteEvent.Parent = game.ReplicatedStorage.Variables
-RemoteEvent.Name = "MatchMakingEvent"
-local Data
+--//Service
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+local Workspace = game:GetService("Workspace")
 
-local PlaceIDS = {
-	Apartment = { Id = 14421495807, Name = "Nagis Apartment" },
-}
+--//Objects
+local Tables = Workspace:WaitForChild("Tables")
 
-local GameData = {
-	EquippedTowers = {"Cure Black","Cure White"},
-	OwnedTowers = {},
-	MaxTowers = 8,
-	Yen = nil,
-}
+local Table_Index = {}
 
-local Towers = {}
---//
+--//Functions
+function JumpEveryone(PlayersTable)
+	for SeatNumber, Getplayer in PlayersTable do
+		if Players:FindFirstChild(Getplayer) then
+			local Character = Players:WaitForChild(Getplayer).Character
+			local Humanoid = Character:WaitForChild("Humanoid")
+
+			Humanoid.Jump = true
+		end
+	end
+end
+
+function MatchTable(GetTables)
+    for _, table in Tables:GetChildren() do
 
 
---// Few things for the Inventory
-script.Enabled = false
+		Table_Index[table.Name] = {
+			Owner = "";
+			["Seats"] = {
+				["Seat1"] = "";
+				["Seat2"] = "";
+				["Seat3"] = "";
+				["Seat4"] = "";
+			}
+		}
+
+		for _, seat in table:GetChildren() do
+			if seat:IsA("Seat") then
+				seat:GetPropertyChangedSignal("Occupant"):Connect(function(...)
+					
+					--/When Player Seat
+					if seat.Occupant ~= nil then
+
+						--/When Owners Join first
+						if Table_Index[table.Name] and Table_Index[table.Name]["Owner"] == "" then
+							Table_Index[table.Name]["Owner"] = seat.Occupant.Parent.Name
+
+							--/Show Ui
+							local PlayerGui = Players:WaitForChild(Table_Index[table.Name]["Owner"]):WaitForChild("PlayerGui")
+							local FindMatchGui = PlayerGui:WaitForChild("ScreenGuis"):WaitForChild("MatchMaking")
+							
+							if FindMatchGui then
+								FindMatchGui.Enabled = true
+							end
+						end
+
+						--/When Player Join
+						if Table_Index[table.Name]["Seats"][seat.Name] then
+							Table_Index[table.Name]["Seats"][seat.Name] = seat.Occupant.Parent.Name
+						end
+
+					else
+
+						--/When player leave from seat, check if his name is same of owner Name, if is all players jump from seat
+						if Table_Index[table.Name] and Table_Index[table.Name]["Seats"][seat.Name] ~= "" and Table_Index[table.Name]["Seats"][seat.Name] == Table_Index[table.Name]["Owner"] then
+							print("Owner Leaves")
+							
+							--/Disable Ui
+							local PlayerGui = Players:WaitForChild(Table_Index[table.Name]["Owner"]):WaitForChild("PlayerGui")
+							local FindMatchGui = PlayerGui:WaitForChild("ScreenGuis"):WaitForChild("MatchMaking")
+							
+							if FindMatchGui then
+								FindMatchGui.Enabled = false
+							end
+							
+							--/Force Player Jump
+							JumpEveryone(Table_Index[table.Name]["Seats"])
+
+							--/Reset Table
+							Table_Index[table.Name] = {
+								Owner = "";
+								["Seats"] = {
+									["Seat1"] = "";
+									["Seat2"] = "";
+									["Seat3"] = "";
+									["Seat4"] = "";
+								}
+							};
+
+						elseif Table_Index[table.Name] and Table_Index[table.Name]["Seats"][seat.Name] then
+							Table_Index[table.Name]["Seats"][seat.Name] = "";
+							print("Player"..Table_Index[table.Name]["Seats"][seat.Name].." Leave from party!")
+						end
+
+						print("Player Leaves")
+					end
+				end)
+			end
+		end
+    end
+end
+
+--//Code
+MatchTable(Tables)
+
 --[[local Datastore = DataStoreService:GetDataStore("PlayersOwnedTowers")
 game.Players.PlayerAdded:Connect(function(player)
 	Data = Datastore:GetAsync(player.UserId)
